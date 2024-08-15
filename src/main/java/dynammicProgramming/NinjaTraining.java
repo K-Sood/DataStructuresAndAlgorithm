a61b9cd0fd75c91b864945af90afcf35687bcc32
soodkartik88 <sood.kartik88@gmail.com>
2024-08-15 23:33:04 +0530

    Updating problem link in ninja training

--- src/main/java/dynammicProgramming/NinjaTraining.java
+++ src/main/java/dynammicProgramming/NinjaTraining.java
@@ -1,5 +1,9 @@
 package main.java.dynammicProgramming;
 
+/**
+ * Problem link : https://www.naukri.com/code360/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+ */
+
 public class NinjaTraining {
 
     public static int ninjaTraining(int n, int points[][]) {
